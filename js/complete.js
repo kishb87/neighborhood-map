807857cbe3a2b2c2b692166353cccb1afe955d9c
kishb87 <kish.bplm@gmail.com>
2015-05-01 17:16:17 -0700

    Create complete application

--- js/complete.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $("input").geocomplete();
-
-    // Trigger geocoding request.
-    $("button.find").click(function(){
-      $("input").trigger("geocode");
-    });
-}
